a5b757f3eec1c949394187dc8faf064db80e0b4e
amsitlab <dezavue3@gmail.com>
2025-02-11 09:28:43 +0700

    Initial commit

--- app/src/main/java/amsitlab/dxplore/Main.java
+++ app/src/main/java/amsitlab/dxplore/Main.java
@@ -2,13 +2,14 @@ package amsitlab.dxplore;
 
 import jadx.api.JadxArgs;
 import jadx.api.JadxDecompiler;
+import java.io.File;
 
 public class Main {
 
    public static void main(String[] args) {
       JadxArgs jadxArgs = new JadxArgs();
-      jadxArgs.setInputFile(new File("classes.dex"));
-      jadxArgs.setOutDir(new File("output"));
+      jadxArgs.setInputFile(new File(args[0]));
+      jadxArgs.setOutDir(new File(args[1]));
       try (JadxDecompiler jadx = new JadxDecompiler(jadxArgs)) {
          jadx.load();
          jadx.save();
