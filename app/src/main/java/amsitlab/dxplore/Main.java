1212183e73c5973d3c3de36759bf7035d9e26144
amsitlab <dezavue3@gmail.com>
2025-02-11 09:27:04 +0700

    Initial commit

--- app/src/main/java/amsitlab/dxplore/Main.java
+++ app/src/main/java/amsitlab/dxplore/Main.java
@@ -1,8 +1,19 @@
 package amsitlab.dxplore;
 
+import jadx.api.JadxArgs;
+import jadx.api.JadxDecompiler;
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("foobar");
-  }
+   public static void main(String[] args) {
+      JadxArgs jadxArgs = new JadxArgs();
+      jadxArgs.setInputFile(new File("classes.dex"));
+      jadxArgs.setOutDir(new File("output"));
+      try (JadxDecompiler jadx = new JadxDecompiler(jadxArgs)) {
+         jadx.load();
+         jadx.save();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
